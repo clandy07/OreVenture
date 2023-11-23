eb84476c458d7d2deba1f939ada9035d55c444cd
clandy07 <127475702+clandy07@users.noreply.github.com>
2023-11-24 01:22:56 +0800

    Added idle animation!

---
+++ OreVenture/src/main/java/main/GamePanel.java
@@ -0,0 +1,95 @@
+package main;
+
+import inputs.KeyboardInputs;
+import inputs.MouseInputs;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class GamePanel extends JPanel {
+    private MouseInputs mouseInputs;
+    private float xDelta = 100, yDelta = 100;
+    private BufferedImage img;
+    private BufferedImage[] idleAni;
+    private int aniTick, aniIndex, aniSpeed = 15;
+
+    public GamePanel() {
+        mouseInputs = new MouseInputs(this);
+
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
+        addMouseListener(mouseInputs);
+        addMouseMotionListener(mouseInputs);
+    }
+
+    private void loadAnimations() {
+        idleAni = new BufferedImage[5];
+
+        for(int i = 0; i < idleAni.length; i++) {
+            idleAni[i] = img.getSubimage(i*64, 0, 64, 40);
+        }
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+        try {
+            img = ImageIO.read(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
+    }
+
+    public void changeXDelta(int value) {
+        this.xDelta += value;
+    }
+
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
+    public void setRecPosition(int x, int y) {
+        this.xDelta = x;
+        this.yDelta = y;
+    }
+
+    private void updateAnimationTick() {
+        aniTick++;
+        if(aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if(aniIndex >= idleAni.length) {
+                aniIndex = 0;
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        updateAnimationTick();
+
+        g.drawImage(idleAni[aniIndex], (int)xDelta, (int)yDelta, 128, 80, null);
+    }
+
+}
